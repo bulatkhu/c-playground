572fcebec4731a9671213ddc692c311c62ec0e03
bulatkhu <hudainazarov.b@gmail.com>
2024-06-06 14:35:08 +0200

    added multiple executables

--- examples/filo/filo.c
+++ examples/filo/filo.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TODO: structs, global variables, etc.
+// struct listelement {
+// 	int value;
+// 	struct listelement *next;
+// };
+// typedef struct listelement FIFO_List; // optional
+// FIFO_List elements;
 
 static int insertElement(int value) {
+	printf("value: %d", value);
+
 	// TODO: implement me!
 	return -1;
 }
@@ -13,14 +20,15 @@ static int removeElement(void) {
 	return -1;
 }
 
-int main (int argc, char* argv[]) {
-	printf("insert 47: %d\n", insertElement(47));
-	printf("insert 11: %d\n", insertElement(11));
-	printf("insert 23: %d\n", insertElement(23));
-	printf("insert 11: %d\n", insertElement(11));
 
-	printf("remove: %d\n", removeElement());
-	printf("remove: %d\n", removeElement());
+int filo (int argc, char* argv[]) {
+	printf("insert 47: %d\n", insertElement(47)); // 47
+	printf("insert 11: %d\n", insertElement(11)); // 11
+	printf("insert 23: %d\n", insertElement(23)); // 23
+	printf("insert 11: %d\n", insertElement(11)); // -1
+
+	printf("remove: %d\n", removeElement()); // 23
+	printf("remove: %d\n", removeElement()); // 11
 
 	// TODO: add more tests
 	exit(EXIT_SUCCESS);
