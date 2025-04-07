d84e3a02082ae36c1cdaad7ba3af3c238ea2865a
bulatkhu <hudainazarov.b@gmail.com>
2025-04-07 22:33:22 +0200

    familiarizing with syntax

--- examples/hello_world/main.c
+++ examples/hello_world/main.c
@@ -4,7 +4,51 @@ void greet_user(void) {
     printf("Hello World!\n");
 }
 
+int func(int b[], size_t len) {
+    return b[len-1];
+}
+
+void basic_syntax() {
+    int numbers[4] = {1,2,3,4};
+    int numbers2[2]; // uninit.
+    numbers2[0] = 23;
+    numbers2[1] = 42;
+    func(numbers, sizeof(numbers));
+    func(numbers2, 2);
+    char string[] = "Hallo Welt";
+    printf("%c\n", string[6]); // ’W’
+    string[6] = 'B';
+    printf("%c\n", string[6]); // ’B’
+    printf("%s\n", string); // ’B’
+}
+
+struct date {
+    int year;
+    int month;
+    int day;
+};
+
+typedef struct date date_t;
+
+void struct_syntax() {
+    struct date today = { 2022, 2, 2 };
+
+    date_t tomorrow = { 2022, 2 };
+    date_t yesterday = {
+        .year = 2022,
+        .month = 2,
+    };
+
+    tomorrow.day = today.day + 1;
+    yesterday.day = today.day - 1;
+
+    printf("tomorrow day: %d\nyesterday day: %d\n", tomorrow.day, yesterday.day);
+}
+
 int main(void) {
-    greet_user();
+    // greet_user();
+    // basic_syntax();
+    struct_syntax();
+
     return 0;
 }
