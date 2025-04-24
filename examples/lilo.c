a9c5f2bc48189f41939b42cddb4bef2d89d1b281
bulatkhu <hudainazarov.b@gmail.com>
2025-04-24 16:06:58 +0200

    impl. lilo

--- examples/lilo.c
+++ examples/lilo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TODO: structs, global variables, etc.
 typedef struct ListNode {
 	int data;
 	struct ListNode* next;
@@ -56,57 +55,43 @@ static int removeElement(void) {
 		return -1;
 	}
 
-	if (currentNode->next->next == NULL) {
-		// d.h. currentNode->next ist das älteste Element in der Liste und wir müssen das entfernen
-		int removeInt = currentNode->next->data;
-		currentNode->next = NULL; // ich entferne das älteste Element indem ich das auf NULL setze
-		free(currentNode->next);
-		return removeInt;
-	}
-
-	while (currentNode->next->next != NULL) {
-		// ich iterere über die Liste bis ich das älteste Element finde
-		currentNode = currentNode->next;
-	}
-	// currentNode->next enthält das älteste Element aus der Liste
-
-	const int removeInt = currentNode->next->data;
-	currentNode->next = NULL;
+	int removeInt = currentNode->next->data;
 	free(currentNode->next);
+	currentNode->next = currentNode->next->next; // das älteste Wert durch den nächsten ältesten Wert ersetzen
 	return removeInt;
 }
 
 int main (int argc, char* argv[]) {
 	printf("insert 47: %d\n", insertElement(47));
-	printListNode();
+	// printListNode();
 	printf("insert 11: %d\n", insertElement(11));
-	printListNode();
+	// printListNode();
 	printf("insert 23: %d\n", insertElement(23));
-	printListNode();
+	// printListNode();
 	printf("insert 11: %d\n", insertElement(11));
-	printListNode();
+	// printListNode();
 
 
 	printf("remove: %d\n", removeElement());
-	printListNode();
+	// printListNode();
 
 	printf("remove: %d\n", removeElement());
-	printListNode();
+	// printListNode();
 
-	printf("remove: %d\n", removeElement());
-	printListNode();
-
-	printf("remove: %d\n", removeElement());
-	printListNode();
-
-	printf("remove: %d\n", removeElement());
-	printListNode();
-
-	printf("insert 9: %d\n", insertElement(9));
-	printListNode();
-
-	printf("remove: %d\n", removeElement());
-	printListNode();
+	// printf("remove: %d\n", removeElement());
+	// printListNode();
+	//
+	// printf("remove: %d\n", removeElement());
+	// printListNode();
+	//
+	// printf("remove: %d\n", removeElement());
+	// printListNode();
+	//
+	// printf("insert 9: %d\n", insertElement(9));
+	// printListNode();
+	//
+	// printf("remove: %d\n", removeElement());
+	// printListNode();
 
 	exit(EXIT_SUCCESS);
 }
